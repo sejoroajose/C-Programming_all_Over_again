925c751dc8f7c110a70317c567cbe3a555cddee0
sejoroajose <ajosesejoro@gmail.com>
2024-03-15 03:35:51 +0100

    More exercises

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	};
+	putchar('\n');
+	return (0);
+
+}
